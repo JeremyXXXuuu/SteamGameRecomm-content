cf39777b3a8b64fc7d5b119ece022ea03d135ace
Jeremy <baodman@icloud.com>
2022-06-17 17:28:13 +0200

    reset router with child process

--- server/controllers/recommendationControllers.js
+++ server/controllers/recommendationControllers.js
@@ -1,12 +1,32 @@
-const getRecommendation = async (req, res) => {
-  const userid = req.user.id;
-  console.log(userid);
-  const { spawn } = require("child_process");
+const asyncHandler = require("express-async-handler");
+const { spawn } = require("child_process");
+const RecomMessage = require("../models/gameRecom.js");
+
+const getRecommendation = asyncHandler(async (req, res) => {
+  const userid = req.user.id.toString();
   const childPython = spawn("python", ["test.py", userid]);
-  childPython.stdout.on("data", (data) => {
-    res.write(data.toString());
-    res.end("end");
+  var recomGames = [];
+  childPython.stdout.on("data", async (data) => {
+    let game = data.toString().split(",");
+    game.pop();
+    game.map((element) => {
+      return Number(element);
+    });
+    try {
+      await Promise.all(
+        game.map(async (r) => {
+          const recomGame = await RecomMessage.find({ app_id: r });
+          var a = recomGame[0];
+
+          recomGames.push(a);
+        })
+      );
+
+      res.status(200).json(recomGames);
+    } catch (error) {
+      console.log(error);
+    }
   });
-};
+});
 
 module.exports = { getRecommendation };
--- server/index.js
+++ server/index.js
@@ -4,13 +4,10 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
 const routes = require("./routes/routes.js");
-const { createProxyMiddleware } = require("http-proxy-middleware");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-const { protect } = require("./middleware/authMiddleware");
-const RecomMessage = require("./models/gameRecom.js");
 
 // app.use(
 //   "/api",
@@ -31,36 +28,8 @@ app.use("/api/recom", routes);
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/game", require("./routes/gameRoutes"));
 app.use("/api/game/test", require("./routes/userRecommRoutes"));
-// app.use("/api/userRecomm", require("./routes/recommendationRoutes"));
+app.use("/api/userRecomm", require("./routes/recommendationRoutes"));
 
-app.use(protect).get("/api/userRecomm", (req, res) => {
-  const { spawn } = require("child_process");
-  const userid = req.user.id.toString();
-  console.log(userid);
-  const childPython = spawn("python", ["test.py", userid]);
-  var recomGames = [];
-  childPython.stdout.on("data", async (data) => {
-    let game = data.toString().split(",");
-    game.pop();
-    game.map((element) => {
-      return Number(element);
-    });
-    try {
-      await Promise.all(
-        game.map(async (r) => {
-          const recomGame = await RecomMessage.find({ app_id: r });
-          var a = recomGame[0];
-
-          recomGames.push(a);
-        })
-      );
-
-      res.status(200).json(recomGames);
-    } catch (error) {
-      console.log(error);
-    }
-  });
-});
 const CONNECTION_URL =
   // "mongodb+srv://jeremy:root@cluster0.5ei45.mongodb.net/database?retryWrites=true&w=majority";
   "mongodb://127.0.0.1:27017/database_DS50";
